b3451154c24aa8428fea70ba571358c1b90ec298
pointp <65812086+MovingPointP@users.noreply.github.com>
2026-04-15 02:45:55 +0900

    fix: Completedのタイポ修正

--- internal/domain/entity/task.go
+++ internal/domain/entity/task.go
@@ -9,7 +9,7 @@ type Task struct {
 	UserID      uint      `json:"user_id" gorm:"not null;index"`
 	Title       string    `json:"title" gorm:"not null"`
 	Description string    `json:"description"`
-	Compleated  bool      `json:"compleated" gorm:"default:false"`
+	Completed   bool      `json:"completed" gorm:"default:false"`
 	CreatedAt   time.Time `json:"created_at"`
 	UpdatedAt   time.Time `json:"updated_at"`
 }
