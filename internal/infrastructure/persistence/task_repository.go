6a8f7bf61dbe65c8772e660cc1c8b1b1883466df
pointp <65812086+MovingPointP@users.noreply.github.com>
2026-04-16 05:05:24 +0900

    add: task_repositoryを追加

---
+++ internal/infrastructure/persistence/task_repository.go
@@ -0,0 +1,50 @@
+package persistence
+
+import (
+	"errors"
+
+	"github.com/MovingPointP/go-task-api/internal/domain/entity"
+	"github.com/MovingPointP/go-task-api/internal/domain/repository"
+	"gorm.io/gorm"
+)
+
+type gormTaskRepository struct {
+	db *gorm.DB
+}
+
+func NewTaskRepository(db *gorm.DB) repository.TaskRepository {
+	return &gormTaskRepository{db: db}
+}
+
+func (r *gormTaskRepository) Create(task *entity.Task) error {
+	return r.db.Create(task).Error
+}
+
+func (r *gormTaskRepository) FindByID(id, userID uint) (*entity.Task, error) {
+	var task entity.Task
+
+	if err := r.db.Where("id = ? AND user_id = ?", id, userID).First(&task).Error; err != nil {
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			return nil, nil
+		}
+		return nil, err
+	}
+	return &task, nil
+}
+
+func (r *gormTaskRepository) FindAllByUserID(userID uint) ([]*entity.Task, error) {
+	var tasks []*entity.Task
+
+	if err := r.db.Where("user_id = ?", userID).Order("created_at DESC").Find(&tasks).Error; err != nil {
+		return nil, err
+	}
+	return tasks, nil
+}
+
+func (r *gormTaskRepository) Update(task *entity.Task) error {
+	return r.db.Updates(task).Error
+}
+
+func (r *gormTaskRepository) Delete(id, userID uint) error {
+	return r.db.Where("id = ? AND user_id = ?", id, userID).Delete(&entity.Task{}).Error
+}
