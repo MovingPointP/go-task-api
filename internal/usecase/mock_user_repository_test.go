b5bde4472800d7cbef20435f3005b67b87c9a37b
pointp <65812086+MovingPointP@users.noreply.github.com>
2026-04-19 03:36:31 +0900

    refactor: mock_user_repositoryのmapのkeyを変更 email->id

--- internal/usecase/mock_user_repository_test.go
+++ internal/usecase/mock_user_repository_test.go
@@ -5,37 +5,37 @@ import (
 )
 
 type mockUserRepository struct {
-	users  map[string]*entity.User
+	users  map[uint]*entity.User
 	nextID uint
 }
 
 func newMockUserRepository() *mockUserRepository {
 	return &mockUserRepository{
-		users:  make(map[string]*entity.User),
+		users:  make(map[uint]*entity.User),
 		nextID: 1,
 	}
 }
 
 func (m *mockUserRepository) Create(user *entity.User) error {
 	user.ID = m.nextID
+	m.users[user.ID] = user
 	m.nextID++
-	m.users[user.Email] = user
 	return nil
 }
 
 func (m *mockUserRepository) FindByEmail(email string) (*entity.User, error) {
-	user, ok := m.users[email]
-	if !ok {
-		return nil, nil
-	}
-	return user, nil
-}
-
-func (m *mockUserRepository) FindByID(id uint) (*entity.User, error) {
 	for _, u := range m.users {
-		if u.ID == id {
+		if u.Email == email {
 			return u, nil
 		}
 	}
 	return nil, nil
+}
+
+func (m *mockUserRepository) FindByID(id uint) (*entity.User, error) {
+	user, ok := m.users[id]
+	if !ok {
+		return nil, nil
+	}
+	return user, nil
 }
